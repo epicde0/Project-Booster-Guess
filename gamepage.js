81cb6b3ec1188abbe43a39f02541d3b069893c15
epicde0 <101399834+epicde0@users.noreply.github.com>
2022-09-30 04:06:36 -0700

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,23 @@
+var y =Math.floor(Math.random()* 10 + 1 );
+player_name = localStorage.getItem("player_name")
+var guess = 1;
+function submit (){
+var x = document.getElementById("guessField").value;
+if (x == y)
+{
+     alert("CONGRATULATIONS!!!"+player_name +"  YOU GUESSED IT RIGHT IN "
+        +guess + "GUESS");
+        guess=1;
+}
+else if (x > y) 
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+     guess++;
+     alert("OOPS SORRY!! TRY A GREATER NUMBER")
+}
+}
+ 
